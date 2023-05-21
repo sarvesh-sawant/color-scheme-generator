9d6eafeb25fa2016506677abf62779df76602b54
Sarvesh Sawant <mailsarveshsawant@gmail.com>
2023-05-21 22:57:50 +0530

    Fix the API URL Creation and call API URL on button click

--- index.js
+++ index.js
@@ -4,15 +4,23 @@ const colorScheme = document.getElementById('get-color-scheme')
 
 const baseUrl = 'https://www.thecolorapi.com'
 const urlEndpoint = '/scheme'
-const queryString = `?hex=${colorWheel.value.substring(1)}&format=json&mode=${dropDownMenu.value}&count=5`
-const completeUrl = baseUrl+urlEndpoint+queryString
 
 let colorSchemes = new Array(5).fill(0)
 
-fetch(completeUrl)
-.then(res => res.json())
-.then(data => {
-    const colors = data.colors
-    colorSchemes = colors.map(color => color.hex.value)
+colorScheme.addEventListener('click', () => {
+
+    let queryString = `?hex=${colorWheel.value.substring(1)}&format=json&mode=${dropDownMenu.value}&count=5`
+    let completeUrl = baseUrl+urlEndpoint+queryString
+
+    fetch(completeUrl)
+    .then(res => res.json())
+    .then(data => {
+        const colors = data.colors
+        colorSchemes = colors.map(color => color.hex.value)
+        console.log(colorSchemes)
+    })
 })
+
+
+
 
